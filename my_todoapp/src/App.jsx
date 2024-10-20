ca295df93644976b646f056d70f2fb0659928a8d
arjunivor <arjunkukreja330@gmail.com>
2024-10-20 16:24:12 +0000

    Initial commit
    new commit

---
+++ my_todoapp/src/App.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import api from "./api";
+import Header from "./Header";
+import TodoContainer from "./TodoContainer";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [todoId, setTodoID] = useState(null);
+
+  const newTodo = {
+    title: searchValue,
+  };
+
+  useEffect(() => {
+    api
+      .get("todos/")
+      .then((res) => {
+        console.log(res.data);
+        setTodos(res.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  function createTodo() {
+    api
+      .post("todos/", newTodo)
+      .then((res) => {
+        setTodos([...todos, res.data]);
+      })
+
+      .catch((err) => console.log(err.message));
+  }
+
+  function updateTodo() {
+    api.put(`todos/${todoId}/`, newTodo)
+    .then(res => {
+      setTodos(todos.map(todo => todo.id===todoId ? res.data : todo ))
+      setTodoID("")
+    })
+    .catch((err) => console.log(err.message));
+  }
+
+
+  function deleteTodo(ID){
+    api.delete(`todos/${ID}/`)
+    .then(res => {
+      setTodos(todos.filter(todo => todo.id !== ID))
+    })
+    .catch(err => console.log(err.message))
+  }
+
+  return (
+    <div className="container">
+      <Header
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        createTodo={createTodo}
+        todoId={todoId}
+        updateTodo={updateTodo}
+      />
+      <TodoContainer
+        todos={todos}
+        setSearchValue={setSearchValue}
+        setTodoID={setTodoID}
+        deleteTodo={deleteTodo}
+      />
+    </div>
+  );
+};
+function handleSubmit(e) {
+    e.preventDefault();
+
+    if (searchValue.length < 3) {
+        return;
+    }
+
+    const data = {
+        title: searchValue,
+        completed: false,
+    };
+
+    if (todoId) {
+        api.put(`/todos/${todoId}/`, data)
+            .then((response) => {
+                // Handle the response from the API
+                console.log(response.data);
+            })
+            .catch((error) => {
+                // Handle any errors
+                console.error(error);
+            });
+    } else {
+        api.post('/todos/', data)
+            .then((response) => {
+                // Handle the response from the API
+                console.log(response.data);
+            })
+            .catch((error) => {
+                // Handle any errors
+                console.error(error);
+            });
+    }
+
+    setSearchValue("");
+}
+export default App;
